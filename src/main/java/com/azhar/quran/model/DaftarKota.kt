772f9fb00ab6e79a0d40458edf300a3b41c5aaaa
robi123-star <77885597+robi123-star@users.noreply.github.com>
2021-01-23 20:50:09 +0700

    Add files via upload

---
+++ src/main/java/com/azhar/quran/model/DaftarKota.kt
@@ -0,0 +1,13 @@
+package com.azhar.quran.model
+
+
+
+class DaftarKota {
+
+    var id: Int? = null
+    var nama: String? = null
+
+    override fun toString(): String {
+        return nama.toString()
+    }
+}
